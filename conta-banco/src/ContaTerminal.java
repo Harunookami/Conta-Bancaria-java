3420207c1bc56e1a5612e76c09f581e44ac943c8
Rafaela <rafinha-2410@hotmail.com>
2024-06-15 00:09:18 -0300

    Resultado final do desafio Conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+         // conhecer e importar a classe scanner
+         // exibir as mensagens para o nosso usuario
+        // obter pela classse scanner os valores digitados no terminal
+        // exibir a mensagem da conta criada 
+
+        int numero = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "Cássio Neves";
+        double saldo = 1566.98;
+
+        System.out.println("Por favor, digite o número da sua conta");
+        String  numeroDigitado = scanner.next();
+        
+        System.out.println("Por favor, digite o número da Agência");
+        String agenciaDigitada = scanner.next();
+        
+        System.out.println("Por favor, digite o seu nome");
+        String nomeClienteDigitado = scanner.next();
+
+
+            System.out.println("Olá, " +  nomeCliente + ". Obrigada por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque." );
+
+
+
     }
 }
